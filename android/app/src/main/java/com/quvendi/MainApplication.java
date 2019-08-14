e2264605197a445dd52f8958e1e9666483db5b0d
victor akinyokun <vicgbadebo@gmail.com>
2019-08-14 07:02:19 +0100

    paystack gateway added and stable

--- android/app/src/main/java/com/quvendi/MainApplication.java
+++ android/app/src/main/java/com/quvendi/MainApplication.java
@@ -3,6 +3,7 @@ package com.quvendi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rnpaystack.RNPaystackPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.rnfs.RNFSPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPaystackPackage(),
             new AsyncStoragePackage(),
           new RNFSPackage(),
           new ImageResizerPackage(),
